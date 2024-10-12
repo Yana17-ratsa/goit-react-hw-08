b414493df1fdb4da202f8b5ea3f9bc8b291c2c2e
Yana17-ratsa <ratsayana13@gmail.com>
2024-10-12 12:32:12 +0300

    refresh edit

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -60,7 +60,7 @@ export const refresh = createAsyncThunk('auth/refresh', async(_, thunkAPI) => {
 
   try {
     setAuthHeader(persistedToken);
-    const res = await axios.get('/users/me');
+    const res = await axios.get('/users/current');
     return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message)
